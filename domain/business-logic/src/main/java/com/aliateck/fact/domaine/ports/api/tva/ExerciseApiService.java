848a441bdd5a886165afebf05ff62be06a6d69e0
A U Thor <allouchi@hotmail.com>
2021-11-25 10:29:19 +0100

    test

---
+++ domain/business-logic/src/main/java/com/aliateck/fact/domaine/ports/api/tva/ExerciseApiService.java
@@ -0,0 +1,19 @@
+package com.aliateck.fact.domaine.ports.api.tva;
+
+import java.util.List;
+
+import com.aliateck.fact.domaine.business.object.Exercise;
+
+public interface ExerciseApiService {
+
+	public Exercise addExercise(Exercise exercise);
+
+	public void deleteExercise(Long id);
+
+	public void updateExercise(Exercise exercise);
+
+	public Exercise findByExercise(String exercice);
+
+	public List<String> findAllExercises();
+
+}
